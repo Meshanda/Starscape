8739eb36e57910ce8b4c2252b5e4818b48206fb9
FranYuu <franois.calvet19@orange.fr>
2023-09-20 15:51:17 +0200

    basic Strate generation

---
+++ Assets/Scripts/WaveFunction/StrateGeneration.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
+
+public class StrateGeneration : MonoBehaviour
+{
+    //public int dimensions;
+    public Vector2Int dimensionsXY;
+    public Vector2Int OffsetXY;
+
+
+    // public List<Cell> gridComponents;
+    // public Cell cellObj;
+    [SerializeField] private List<Strate> _strates;
+    public TileBase tile;
+    public Tilemap tileGround;
+
+    public TileBase[] tileObjects;
+
+
+    int iterations = 0;
+
+    void Awake()
+    {
+        //gridComponents = new List<Cell>();
+        InitializeGrid();
+    }
+    
+    void InitializeGrid()
+    {
+        //for (int y = 0; y < dimensionsXY.y; y++)
+        //{
+        //    for (int x = 0; x < dimensionsXY.x; x++)
+        //    {
+        //        tileGround.SetTile(new Vector3Int(x, -y), tile);
+        //    }
+        //}
+        int debutStrate = 0;
+        for ( int s = 0 ; s < _strates.Count(); s++) 
+        {
+            
+
+            if(s > 0) 
+            {
+                debutStrate += _strates[s - 1].SizeY ;
+                Debug.Log(debutStrate);
+            }
+            for (int y = debutStrate; y < debutStrate+_strates[s].SizeY; y++)
+            {
+                for (int x = 0; x < dimensionsXY.x; x++)
+                {
+                    tileGround.SetTile(new Vector3Int(x- OffsetXY.x, -y- OffsetXY.y), GetTileFromStrate(_strates[s]));
+                }
+            }
+        }
+        
+
+
+        //StartCoroutine(CheckEntropy());
+    }
+
+    public TileBase GetTileFromStrate(Strate strate) 
+    {
+        float rng = Random.Range(0, 100);
+        int pourcentage = 0;
+        for (int i = 0; i < strate.Tiles.Count(); i++) 
+        {
+            pourcentage  += strate.Tiles[i].Pourcentage;
+            if (rng < pourcentage) 
+            {
+                return strate.Tiles[i].Tile;
+            }
+        }
+        return strate.Tiles[0].Tile;
+    }
+}
+[Serializable]
+public struct Strate 
+{
+   public TilesStrate[] Tiles;
+   public int SizeY;
+}
+
+[Serializable]
+public struct TilesStrate 
+{
+    public TileBase Tile;
+    public int Pourcentage;
+}
