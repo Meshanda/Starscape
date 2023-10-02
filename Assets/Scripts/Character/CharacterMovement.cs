ce5788f76f5771db376c9b8dc45e00d55921d36a
Lionel <li.janin@laposte.net>
2023-10-02 17:13:28 +0200

    fix warning (mb)

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CharacterController))]
 public class CharacterMovement : MonoBehaviour
 {
 
