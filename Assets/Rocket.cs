a88195164d45f5fa01c5b4bedc7f9e044c59c412
Lionel <li.janin@laposte.net>
2023-10-04 17:40:22 +0200

    ad rocket + fix block placing on character

---
+++ Assets/Rocket.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] private Tilemap _platformMap;
+    [SerializeField] private Transform _platformPos;
+
+    private Tilemap GroundTile => World.Instance.GroundTilemap;
+
+    private void Start()
+    {
+        
+    }
+
+    public void CreatePlatform()
+    {
+        var pos = GroundTile.WorldToCell(_platformPos.position);   
+        
+        for (int i = _platformMap.cellBounds.min.x - 1; i < _platformMap.cellBounds.max.x + 1; i++)
+        {
+            for (int j = _platformMap.cellBounds.min.y - 1; j < _platformMap.cellBounds.max.y + 1; j++)
+            {
+                TileBase tile = _platformMap.GetTile(new Vector3Int(i, j));
+                if (tile != null) 
+                {
+                    GroundTile.SetTile(new Vector3Int(i, j) + pos, tile);
+                }
+            }
+        }
+    }
+}
